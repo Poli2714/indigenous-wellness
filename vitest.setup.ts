60510a6b750814d33596113124a0fb698d7b2318
Elgun Mehdi <elgun88@gmail.com>
2026-04-26 20:17:37 -0400

    test: update Vitest configuration

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,13 @@
-// Any setup scripts you might need go here
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-dom/vitest';
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
 
 // Load .env files
 import 'dotenv/config';
